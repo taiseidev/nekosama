4f294b3286a38b768606f93f3b4479074a2d3e61
onishi taisei <taisei.onishi@axia-works.com>
2022-05-15 15:00:06 +0900

    🚀 feat: Add logger file

---
+++ application/lib/utils/logger_util.dart
@@ -0,0 +1,25 @@
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
+import 'package:roggle/roggle.dart';
+
+class LoggerUtil {
+  static final logger = Roggle.crashlytics(
+    printer: CrashlyticsPrinter(
+      errorLevel: Level.error, // Logs above this level will call onError
+      onError: (event) {
+        // Send an error to Firebase Crashlytics as follows
+        FirebaseCrashlytics.instance.recordError(
+          event.exception,
+          event.stack,
+          fatal: true,
+        );
+      },
+      onLog: (event) {
+        // Send logs to Firebase Crashlytics as follows
+        FirebaseCrashlytics.instance.log(event.message);
+      },
+      loggerName: 'Roggle',
+      levelEmojis: {}, // Replace level emojis map
+      levelLabels: {}, // Replace level labels map
+    ),
+  );
+}
