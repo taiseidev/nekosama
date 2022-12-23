e2729f46f92591e8c32dd975deec5a553f09ec11
onishi taisei <taisei.developer@gmail.com>
2022-12-23 22:44:53 +0900

    🚀 feat: Fix screen vertically

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:nekosama/app.dart';
 import 'package:nekosama/firebase_options_dev.dart' as dev;
 
 Future<void> main() async {
@@ -8,26 +10,16 @@ Future<void> main() async {
   const flavor = String.fromEnvironment('FLAVOR');
   debugPrint('環境：$flavor');
 
-  // Flavor に応じた FirebaseOptions を準備する
-  // final firebaseOptions = flavor == 'prod'
-  //     ? prod.DefaultFirebaseOptions.currentPlatform
-  //     : dev.DefaultFirebaseOptions.currentPlatform;
-
+  // prodが動かないのでいったん決め打ちでdevを使用
   final firebaseOptions = dev.DefaultFirebaseOptions.currentPlatform;
 
   // Firebase の初期化
-  await Firebase.initializeApp(
-    options: firebaseOptions,
-  );
+  await Firebase.initializeApp(options: firebaseOptions);
+
+  // 画面の向きを固定する。
+  await SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
 
   runApp(
-    MyApp(),
+    const App(),
   );
-}
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp();
-  }
 }
