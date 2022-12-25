23e431e189adb489ac145c9ae4519037f407ac52
onishi taisei <taisei.developer@gmail.com>
2022-12-25 19:40:37 +0900

    🚀 feat: Create a Widget that adds background by default

---
+++ lib/ui/widgets/stack_with_background.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:nekosama/ui/widgets/background.dart';
+
+// デフォルトのバックグラウンドをStack
+class StackWithBackground extends StatelessWidget {
+  const StackWithBackground({
+    super.key,
+    required this.child,
+  });
+
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        const PrimaryBackground(),
+        child,
+      ],
+    );
+  }
+}
