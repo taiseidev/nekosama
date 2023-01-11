a6f347518800fa4848f31c7c3ad32ef941fab1d6
onishi taisei <taisei.developer@gmail.com>
2023-01-11 22:21:58 +0900

    🚜 refactor: log

--- lib/src/utils/logger.dart
+++ lib/src/utils/logger.dart
@@ -1,12 +1,7 @@
-import 'dart:io';
-
 import 'package:roggle/roggle.dart';
 
 const _loggerName = '[APP]';
 
-/// ロガー
-///
-/// どこから呼び出してOK
 /// ログレベル毎の使い分けは次の通り
 /// verbose: 垂れ流し系、大量に出力される場合
 /// debug: デバッグ時に一時的に使う場合
@@ -19,8 +14,6 @@ final logger = Roggle(
     loggerName: _loggerName,
     // error 以上のときはスタックトレースを出力する
     stackTraceLevel: Level.error,
-    // iOS はカラー非対応
-    colors: !Platform.isIOS,
     // ログが長くなるので非表示
     printCaller: false,
   ),
